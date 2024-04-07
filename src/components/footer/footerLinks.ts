570a9f66bb5c6a83f53c42d72c6121b2dc66093f
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-04-07 12:17:25 +0530

    created the footer

---
+++ src/components/footer/footerLinks.ts
@@ -0,0 +1,10 @@
+import { NavigationLinks } from "@/types/Links";
+
+const links: NavigationLinks[] = [
+  { name: "Home", to: "/" },
+  { name: "About Us", to: "/aboutus" },
+  { name: "Workshops", to: "/workshops" },
+  { name: "Projects", to: "/projects" },
+];
+
+export default links;
