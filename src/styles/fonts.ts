0b56287088b9fa35f2160234f6938a2b7060343e
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-04-07 12:15:54 +0530

    added the fonts and interfaces

---
+++ src/styles/fonts.ts
@@ -0,0 +1,70 @@
+import {
+  Inter,
+  Inika,
+  Alata,
+  Judson,
+  Kaisei_Opti,
+  DM_Sans,
+  Amaranth,
+  Montserrat,
+} from "next/font/google";
+
+export const f_montserrat_800 = Montserrat({
+  weight: "900",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const f_inter_800 = Inter({
+  weight: "800",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const f_inter_700 = Inter({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const f_inter_400 = Inter({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const f_inika_700 = Inika({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const f_alata_400 = Alata({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const f_judson_700 = Judson({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const f_kaiseiOpti_700 = Kaisei_Opti({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const f_dmsans_700 = DM_Sans({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const f_amaranth_400 = Amaranth({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
--- src/types/footerlinsk.ts
+++
@@ -1,5 +0,0 @@
-export type footerlinks{
-    id:number,
-    path:string,
-    name:string,
-}
