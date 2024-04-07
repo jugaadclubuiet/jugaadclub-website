972c3ab9802a5ce204b61374de58de53ef153d76
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-04-07 12:19:45 +0530

    exported all the required components through componentExporter

--- src/components/ComponentExporter.ts
+++ src/components/ComponentExporter.ts
@@ -1,7 +1,13 @@
-export { default as HeroSection } from "./hero/index";
+export { default as Navbar } from "./navbar/index";
+
+export { default as HeroPage } from "./hero/index";
+
+export { default as MastHead } from "./masthead/index";
+export { default as Info } from "./info/index";
+export { default as Worshops } from "./workshops/index";
+export { default as Projects } from "./projects/index";
+export { default as Sidebar } from "./sidebar/index";
+
 export { default as About } from "./About/index";
-export { default as info } from "./info/index";
-export { default as sidebar } from "./sidebar/index";
-export { default as projects } from "./projects/index";
-export { default as footer } from "./footer/index";
-export {default as Navbar} from "./navbar/index";
+
+export { default as Footer } from "./footer/index";
