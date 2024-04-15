0766fda62fa45547f110048d7c4fc2d26bbd6ff1
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-04-15 22:50:40 +0530

    added the flip image feature in achievements section

--- src/components/About/Achievement/Achievements.tsx
+++ src/components/About/Achievement/Achievements.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import { ImageType } from "@/types/Image";
 import { f_alata_400 } from "@/styles/fonts";
+import { AchievementsImages } from "./Data";
+import "@/styles/flip_card.css";
 
 const Achievements = () => {
-  const Images: ImageType[] = [
-    { src: "/team/achievements.jpg", alt: "1", title: "Achievement 1" },
-    { src: "/team/achievements.jpg", alt: "2", title: "Achievement 2" },
-    { src: "/team/achievements.jpg", alt: "3", title: "Achievement 3" },
-  ];
-
   return (
     <>
       <div className="w-full flex flex-col items-center py-5 px-4">
@@ -23,16 +18,36 @@ const Achievements = () => {
           Achievements
         </h1>
         <div className="flex flex-row items-start justify-evenly w-full my-2">
-          {Images.map((image, index) => (
-            <Image
-              key={index}
-              src={image.src}
-              alt={image.alt}
-              width={250}
-              height={400}
-              draggable={false}
-              className="border-2 border-solid border-white rounded-lg w-80 h-[380px]"
-            />
+          {AchievementsImages.map((image, index) => (
+            <div className="flip-card" key={index}>
+              <div className="flip-card-inner w-full h-full">
+                <div className="flip-card-front w-full h-full">
+                  <Image
+                    src={image.front.src}
+                    alt={image.front.alt}
+                    width={300}
+                    height={300}
+                    draggable={false}
+                    className={`w-full h-full border-4 rounded-lg border-solid border-white`}
+                  />
+                </div>
+                <div className="flip-card-back w-full h-full flex flex-col items-center justify-between bg-black">
+                  <Image
+                    src={image.back.src}
+                    alt={image.back.alt}
+                    width={300}
+                    height={300}
+                    draggable={false}
+                    className={`w-full h-full px-5 py-4`}
+                  />
+                  <p
+                    className={`text-white text-sm p-2 ${f_alata_400} text-center w-full rounded-md`}
+                  >
+                    {image.back.title}
+                  </p>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       </div>
---
+++ src/components/About/Achievement/Data.ts
@@ -0,0 +1,45 @@
+import { DuoImageType } from "@/types/Image";
+
+export const AchievementsImages: DuoImageType[] = [
+  {
+    front: {
+      src: "/team/achievements.jpg",
+      alt: "1",
+      title: "Achievement 1",
+    },
+    back: {
+      src: "/team/1.jpg",
+      alt: "back 1",
+      title:
+        "Jugaad Robotics Club secured first position at Techzibition, IIT Roorkee, showcasing Faraday Station project.",
+    },
+  },
+  {
+    front: {
+      src: "/team/achievements.jpg",
+      alt: "2",
+      title:
+        "Jugaad Robotics Club secured first position at Techzibition, IIT Roorkee, showcasing Faraday Station project.",
+    },
+    back: {
+      src: "/team/2.jpg",
+      alt: "back 2",
+      title:
+        "Jugaad Robotics Club secured first position at Techzibition, IIT Roorkee, showcasing Faraday Station project.",
+    },
+  },
+  {
+    front: {
+      src: "/team/achievements.jpg",
+      alt: "3",
+      title:
+        "Jugaad Robotics Club secured first position at Techzibition, IIT Roorkee, showcasing Faraday Station project.",
+    },
+    back: {
+      src: "/team/3.jpg",
+      alt: "back 3",
+      title:
+        "Jugaad Robotics Club secured first position at Techzibition, IIT Roorkee, showcasing Faraday Station project.",
+    },
+  },
+];
