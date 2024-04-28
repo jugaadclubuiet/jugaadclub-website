6e4fc977da296dfa2de6e974fdfd903f2097e26c
Riya <riyabansal2103@gmail.com>
2024-04-28 23:34:12 +0530

    added designers details

--- src/components/footer/avatar/designers.tsx
+++ src/components/footer/avatar/designers.tsx
@@ -5,7 +5,7 @@ const Avatars = () => {
     <div className={styles.avatars}>
         <div className={styles.user}>
             <div className={styles.avatarsitem}>
-                <Image src="/members/user.jpg" alt="Riya" width={200} height={200} className={styles.avatar} />
+                <Image src="/members/riyaimg.jpg" alt="Riya" width={200} height={200} className={styles.avatar} />
             </div>
             <span className={styles.title}>Riya </span>
             <span className={styles.position}>TeamLead </span>
@@ -13,30 +13,30 @@ const Avatars = () => {
         </div>
         <div className={styles.user}>
             <div className={styles.avatarsitem}>
-                <Image src="/members/user.jpg" alt="Karan" width={200} height={200} className={styles.avatar} />
+                <Image src="/members/karan.jpg" alt="Karan" width={200} height={200} className={styles.avatar} />
             </div>
             <div className={styles.title}>Karan</div>
             <span className={styles.position}>Developer </span>
         </div>
         <div className={styles.user}>
             <div className={styles.avatarsitem}>
-                <Image src="/members/user.jpg" alt="Nancy"width={200} height={200} className={styles.avatar} />
+                <Image src="/members/nancy.jpg" alt="Nancy"width={200} height={200} className={styles.avatar} />
             </div>
             <div className={styles.title}>Nancy</div>
             <span className={styles.position}>Designer</span>
         </div>
         <div className={styles.user}>
             <div className={styles.avatarsitem}>
-                <Image src="/members/user.jpg" alt="Navya" width={200} height={200} className={styles.avatar}/>
+                <Image src="/members/navya.jpg" alt="Navya" width={200} height={200} className={styles.avatar}/>
             </div>
             <div className={styles.title}>Navya<br/></div>
             <span className={styles.position}>Designer</span>
         </div>
-        <div className={styles.user}>
+        {/* <div className={styles.user}>
             <div className={styles.avatarsitem}>
                 <Image src="/members/user.jpg" alt="" width={200} height={200} className={styles.avatar} />
             </div>
-        </div>
+        </div> */}
     </div>
   );
 };
